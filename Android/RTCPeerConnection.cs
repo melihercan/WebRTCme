2f1b4226fa1732a1b853890ab94c4266e896fd6d
Melih Ercan <melihercan@gmail.com>
2020-10-03 12:32:38 +0300

    Add project files.

---
+++ Android/RTCPeerConnection.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using WebRTCme;
+
+namespace WebRtc.Android
+{
+    internal class RTCPeerConnection : IRTCPeerConnection
+    {
+        public void Do()
+        {
+            Console.WriteLine("I am Android");
+        }
+    }
+}
---
+++ DemoApp/Mobile/DemoApp/DemoApp/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using Xamarin.Forms;
+using DemoApp.ViewModels;
+
+namespace DemoApp.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
