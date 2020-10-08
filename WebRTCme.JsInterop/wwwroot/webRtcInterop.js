1ac64f95727a06839e07ff6af2977b41237b47e2
Melih Ercan <melihercan@gmail.com>
2020-10-08 18:25:23 +0300

    JS hiding private stuff.

--- WebRTCme.JsInterop/wwwroot/webRtcInterop.js
+++ WebRTCme.JsInterop/wwwroot/webRtcInterop.js
@@ -1,24 +1,55 @@
-﻿// Use same value on .NET side.
-const jsObjectRefKey = '__jsObjectRefId';
+﻿webRtcInterop = (function () {
 
-var jsObjectRefs = {};
-var jsObjectRefId = 0;
+    let public = {};
 
-DotNet.attachReviver(function (key, value) {
-    if (value && typeof value === 'object' && value.hasOwnProperty(jsObjectRefKey) &&
-        typeof value[jsObjectRefKey] === 'number') {
-        let id = value[jsObjectRefKey];
-        if (!(id in jsObjectRefs)) {
-            throw new Error("JS object reference with id:" + id + " does not exist");
+    // Use same value on .NET side.
+    const jsObjectRefKey = '__jsObjectRefId';
+
+    let jsObjectRefs = {};
+    let jsObjectRefId = 0;
+
+    DotNet.attachReviver(function (key, value) {
+        if (value && typeof value === 'object' && value.hasOwnProperty(jsObjectRefKey) &&
+            typeof value[jsObjectRefKey] === 'number') {
+            let id = value[jsObjectRefKey];
+            if (!(id in jsObjectRefs)) {
+                throw new Error('JS object reference with id:' + id + ' does not exist');
+            }
+            return jsObjectRefs[id];
+        } else {
+            return value;
         }
-        return jsObjectRefs[id];
-    } else {
-        return value;
-    }
-})
+    })
 
-window.webRtcInterop = {
-    initialize: function (msg) {
-        console.log(msg);
+    addJsObjectRef = function (object) {
+        let id = jsObjectRefId++;
+        jsObjectRefs[id] = object;
+        let jsObjectRef = {};
+        jsObjectRef[jsObjectRefKey] = id;
+        return jsObjectRef;
     }
-}
+
+    public.getProperty = function (object, property) {
+        if (object === null) {
+            object = window;
+        }
+        let list = property.replace('[', '.').replace(']', '').split('.');
+        if (list[0] === "") {
+            list.shift();
+        }
+        let current = object;
+        for (i = 0; i < list.length; i++) {
+            if (list[i] in current) {
+                current = current[list[i]];
+            } else {
+                return null;
+            }
+        }
+
+        let ret = addJsObjectRef(current);
+        return ret;
+    }
+
+    return public;
+
+})();
