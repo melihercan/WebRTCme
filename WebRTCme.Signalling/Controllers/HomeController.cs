f59be7fe318a260f82bf2d8a144554a0ac13556d
Melih Ercan <melihercan@gmail.com>
2020-10-13 15:32:06 +0300

    Added Signalling server project.

---
+++ WebRTCme.Signalling/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace WebRtcSignalling.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public /*IActionResult*/ ContentResult Index()
+        {
+            return Content("Hello World!");
+        }
+
+    }
+}
