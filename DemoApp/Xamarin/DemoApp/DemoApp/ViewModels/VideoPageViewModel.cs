fe6bb4c36b44769277b8e961295bc7cb46c87221
Melih Ercan <melihercan@gmail.com>
2020-10-27 20:56:12 +0300

    Added new properties to Video control.

--- DemoApp/Xamarin/DemoApp/DemoApp/ViewModels/VideoPageViewModel.cs
+++ DemoApp/Xamarin/DemoApp/DemoApp/ViewModels/VideoPageViewModel.cs
@@ -1,10 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
 using System.Text;
 
 namespace DemoApp.ViewModels
 {
-    public class VideoPageViewModel
+    public class VideoPageViewModel : INotifyPropertyChanged
     {
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void OnPropertyChanged([CallerMemberName] string name = null) => 
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+
+        private string _localSource;
+        public string LocalSource
+        {
+            get => _localSource;
+            set
+            {
+                _localSource = value;
+                OnPropertyChanged();
+            }
+        }
+
     }
 }
--- Gui/WebRTCme.Gui.Xamarin/Video.cs
+++ Gui/WebRTCme.Gui.Xamarin/Video.cs
@@ -7,8 +7,22 @@ namespace WebRtcGuiXamarin
 {
     public class Video : View       
     {
+        public enum TypeEnum
+        {
+            Local,
+            Remote,
+            MP4
+        }
+
         public static readonly BindableProperty TestProperty = BindableProperty
             .Create("Test", typeof(VideoOptions), typeof(Video), VideoOptions.Option1);
+
+        public static readonly BindableProperty TypeProperty = BindableProperty
+            .Create(nameof(TypeProperty), typeof(TypeEnum), typeof(Video), TypeEnum.Local);
+
+        public static readonly BindableProperty SourceProperty = BindableProperty
+            .Create(nameof(SourceProperty), typeof(string), typeof(Video), "Default");
+        
 
         public VideoOptions Options
         {
@@ -16,5 +30,16 @@ namespace WebRtcGuiXamarin
             set => SetValue(TestProperty, value);
         }
 
+        public TypeEnum Type
+        {
+            get => (TypeEnum)GetValue(TypeProperty);
+            set => SetValue(TypeProperty, value);
+        }
+
+        public string Source
+        {
+            get => (string)GetValue(SourceProperty);
+            set => SetValue(SourceProperty, value);
+        }
     }
 }
