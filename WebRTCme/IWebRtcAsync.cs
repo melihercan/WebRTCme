b5518c3f21fb09ebab5d004d197fe869268d4964
Melih Ercan <melihercan@gmail.com>
2020-11-25 16:18:33 +0300

    Started with all Async API.

---
+++ WebRTCme/IWebRtcAsync.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.JSInterop;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebRTCme
+{
+    public interface IWebRtcAsync
+    {
+        Task<IWindow> WindowAync(IJSRuntime jsRuntime = null);
+
+        Task CleanupAsync();
+    }
+}
