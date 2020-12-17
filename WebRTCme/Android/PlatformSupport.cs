1e0b7d5a0e80805b4247ccaefcd0f2e42abe466e
Melih Ercan <melihercan@gmail.com>
2020-12-17 17:03:31 +0300

    Android platform specific CreateCameraView in progress...

--- WebRTCme/Android/PlatformSupport.cs
+++ WebRTCme/Android/PlatformSupport.cs
@@ -22,18 +22,36 @@ namespace WebRTCme
             ///            var nativeCameraVideoCapturer =
             var nativeCameraVideoCapturer = new Webrtc.Camera2Capturer(context, track.Id, null);
 
+            var eglBaseContext = EglBaseHelper.Create().EglBaseContext;
+
+
             nativeCameraVideoCapturer.Initialize(Webrtc.SurfaceTextureHelper.Create(
                 "CaptureThread",
-                EglBaseHelper.Create().EglBaseContext),
+                eglBaseContext),
                 context, 
                 nativeVideoSource.CapturerObserver);
 
 
             nativeCameraVideoCapturer.StartCapture(100, 100, 30);
 
-            var nativeLocalVideoTrack = track as Webrtc.VideoTrack;
 
-            ////nativeLocalVideoTrack.AddSink(new VideoRendererProxy());
+            var nativeSurfaceViewRenderer = new Webrtc.SurfaceViewRenderer(context);
+            nativeSurfaceViewRenderer.Init(eglBaseContext, null);
+            
+            
+            var nativeLocalVideoTrack = track.NativeObject as Webrtc.VideoTrack;
+
+            var nativeVideoSink = new VideoRendererProxy();
+            nativeVideoSink.Renderer = nativeSurfaceViewRenderer;
+
+            try
+            {
+                nativeLocalVideoTrack.AddSink(new VideoRendererProxy());
+            }
+            catch(Exception ex)
+            {
+                var x = ex.Message;
+            }
 
 
 
@@ -42,14 +60,20 @@ namespace WebRTCme
 
 
 
-            return null;
+            return nativeSurfaceViewRenderer;
         }
     }
 
     public class VideoRendererProxy : Java.Lang.Object, Webrtc.IVideoSink
     {
         private Webrtc.IVideoSink _renderer;
-        public object NativeObject => this;
+
+////        public VideoRendererProxy()
+////        {
+////            _renderer = this;
+////        }
+
+////        public object NativeObject => this;
 
         public Webrtc.IVideoSink Renderer
         {
@@ -57,7 +81,7 @@ namespace WebRTCme
             set
             {
                 if (_renderer == this)
-                    throw new InvalidOperationException("You can set renderer to self");
+                    throw new InvalidOperationException("You can not set renderer to self");
                 _renderer = value;
             }
         }
