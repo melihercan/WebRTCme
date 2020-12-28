ee1e3d8348cb8f856b2dbf0cd5435d822ac065c2
Melih Ercan <melihercan@gmail.com>
2020-12-28 13:59:14 +0300

    Xirsys server parmeters.

--- WebRTCme.SignallingServer/TurnServerService/XirsysClient.cs
+++ WebRTCme.SignallingServer/TurnServerService/XirsysClient.cs
@@ -1,16 +1,37 @@
-﻿using System.Threading.Tasks;
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
 
 namespace WebRTCme.SignallingServer.TurnServerService
 {
     public class XirsysClient : ITurnServerClient
     {
-        public XirsysClient()
+        private readonly IHttpClientFactory _httpClientFactory;
+        private readonly IConfiguration _configuration;
+
+        public XirsysClient(IHttpClientFactory httpClientFactory, IConfiguration configuration)
         {
+            _httpClientFactory = httpClientFactory;
+            _configuration = configuration;
         }
 
-        public Task<RTCIceServer[]> GetIceServers()
+        public async Task<RTCIceServer[]> GetIceServers()
         {
-            throw new System.NotImplementedException();
+            var httpClient = _httpClientFactory.CreateClient();
+            httpClient.BaseAddress = new Uri(new Uri(_configuration["TurnServerUrl:Xirsys"]),
+                _configuration["TurnServerChannel:Xirsys"]);
+            httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue(
+                "Basic", _configuration["TurnServerAuthorization:Xirsys"]);
+            httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+            var response = await httpClient.PutAsync(string.Empty, new StringContent(string.Empty))
+                .ConfigureAwait(false);
+            response.EnsureSuccessStatusCode();
+
+
+            return null;
         }
     }
 }
