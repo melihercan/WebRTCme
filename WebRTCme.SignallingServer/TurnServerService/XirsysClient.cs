eec10bddb09532d3bd45d9daa3ddbd8110425100
Melih Ercan <melihercan@gmail.com>
2020-12-28 13:41:25 +0300

    Revert "Getting ICE servers from XirSis."
    This reverts commit 0d22aad58e12e8c7b301d794168cf55d85976b8a.

--- WebRTCme.SignallingServer/TurnServerService/XirsysClient.cs
+++ WebRTCme.SignallingServer/TurnServerService/XirsysClient.cs
@@ -1,37 +1,16 @@
-﻿using Microsoft.Extensions.Configuration;
-using System;
-using System.Net.Http;
-using System.Net.Http.Headers;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 
 namespace WebRTCme.SignallingServer.TurnServerService
 {
     public class XirsysClient : ITurnServerClient
     {
-        private readonly IHttpClientFactory _httpClientFactory;
-        private readonly IConfiguration _configuration;
-
-        public XirsysClient(IHttpClientFactory httpClientFactory, IConfiguration configuration)
+        public XirsysClient()
         {
-            _httpClientFactory = httpClientFactory;
-            _configuration = configuration;
         }
 
-        public async Task<RTCIceServer[]> GetIceServers()
+        public Task<RTCIceServer[]> GetIceServers()
         {
-            var httpClient = _httpClientFactory.CreateClient();
-            httpClient.BaseAddress =  new Uri(new Uri(_configuration["TurnServerUrl:Xirsys"]), 
-                _configuration["TurnServerChannel:Xirsys"]);
-            httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue(
-                "Basic", _configuration["TurnServerAuthorization:Xirsys"]);
-            httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-
-            var response = await httpClient.PutAsync(string.Empty, new StringContent(string.Empty))
-                .ConfigureAwait(false);
-            response.EnsureSuccessStatusCode();
-             
-
-            return null;
+            throw new System.NotImplementedException();
         }
     }
 }
