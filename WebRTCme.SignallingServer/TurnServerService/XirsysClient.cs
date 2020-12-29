ba35a1cab1cdb9086913da780c1dac2cce87f20b
Melih Ercan <melihercan@gmail.com>
2020-12-29 15:45:22 +0300

    Got ice servers from Xirsys server.

--- WebRTCme.SignallingServer/TurnServerService/XirsysClient.cs
+++ WebRTCme.SignallingServer/TurnServerService/XirsysClient.cs
@@ -1,8 +1,11 @@
 ﻿using Microsoft.Extensions.Configuration;
 using System;
+using System.Linq;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace WebRTCme.SignallingServer.TurnServerService
@@ -29,13 +32,66 @@ namespace WebRTCme.SignallingServer.TurnServerService
                     Encoding .GetEncoding(28591).GetBytes(_configuration["TurnServerAuthorization:Xirsys"])));
             httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
 
+#if false
+            var x1 = new XirsysTurnResponse
+            {
+                S = XirsysTurnStatusResponse.Ok,
+                V = new XirsysTurnValueResponse
+                {
+                    IceServers = new XirsysTurnIceServerResponse[]
+                    {
+                        new XirsysTurnIceServerResponse
+                        {
+                            Url = "stun:eu-turn5.xirsys.com"
+                        },
+                        new XirsysTurnIceServerResponse
+                        {
+                            Username = "0QhBDZyu8dIqP8oEY10ZxmZppbmxfYZWBXxoMtrHqTV8mrsZIZ4iMfzfkHM_CtCvAAAAAF_rA5VtZWxpaGVyY2FuNDg=",
+                            Url = "turn:eu-turn5.xirsys.com:80?transport=udp",
+                            Credential = "de142dd0-49bf-11eb-9ec6-0242ac140004"
+                        },
+                        new XirsysTurnIceServerResponse
+                        {
+                            Username = "0QhBDZyu8dIqP8oEY10ZxmZppbmxfYZWBXxoMtrHqTV8mrsZIZ4iMfzfkHM_CtCvAAAAAF_rA5VtZWxpaGVyY2FuNDg=",
+                            Url = "turn:eu-turn5.xirsys.com:3478?transport=udp",
+                            Credential = "de142dd0-49bf-11eb-9ec6-0242ac140004"
+                        }
+                    }
+                }
+            };
+            var o = new JsonSerializerOptions
+            {
+                PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+                DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull,
+                Converters = { new JsonStringEnumConverter(JsonNamingPolicy.CamelCase) }
+            };
+            var x2 = JsonSerializer.Serialize(x1, o);
+            var x3 = JsonSerializer.Deserialize<XirsysTurnResponse>(x2, o);
+#endif
+
+
             var response = await httpClient.PutAsync(string.Empty, new StringContent(string.Empty));
             response.EnsureSuccessStatusCode();
 
-            var content = await response.Content.ReadAsStringAsync();
+            var json = await response.Content.ReadAsStringAsync();
+            var xirsysTurnResponse = JsonSerializer.Deserialize<XirsysTurnResponse>(json, new JsonSerializerOptions
+            {
+                PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+                DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull,
+                Converters = { new JsonStringEnumConverter(JsonNamingPolicy.CamelCase) }
+            });
+            if (xirsysTurnResponse.S == XirsysTurnStatusResponse.Error)
+                throw new Exception("Xirsys turn server reported error");
 
-
-            return null;
+            var iceServers = xirsysTurnResponse.V.IceServers
+                .Select(xirsisIceServer => new RTCIceServer 
+                { 
+                    Credential = xirsisIceServer.Credential,
+                    CredentialType = RTCIceCredentialType.Password,
+                    Urls = new string[] { xirsisIceServer.Url },
+                    Username = xirsisIceServer.Username
+                }).ToArray();
+            return iceServers;
         }
     }
 }
---
+++ WebRTCme.SignallingServer/TurnServerService/XirsysTurnResponse.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace WebRTCme.SignallingServer.TurnServerService
+{
+        //[JsonConverter(typeof(JsonStringEnumConverter))JsonNamingPolicy.CamelCase)))]
+    [JsonConverter(typeof(JsonStringEnumConverter))]
+    public enum XirsysTurnStatusResponse
+    {
+        Ok,
+        Error
+    }
+
+    public class XirsysTurnIceServerResponse
+    {
+        public string Username { get; set; }
+        public string Url { get; set; }
+        public string Credential { get; set; }
+    }
+
+    public class XirsysTurnValueResponse
+    {
+        public XirsysTurnIceServerResponse[] IceServers { get; set; }
+    }
+    public class XirsysTurnResponse
+    {
+        public XirsysTurnValueResponse V { get; set; }
+        public XirsysTurnStatusResponse S { get; set; }
+    }
+}
