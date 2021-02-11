93199a3f7fde01286bc6e602819b2a91a1a5fe9a
Melih Ercan <melihercan@gmail.com>
2021-02-11 17:44:17 +0300

    Ignore null in JSInterop JSON globally.

--- WebRTCme.DemoApp/Blazor/WebRTCMe.DemoApp.Blazor.Wasm/Program.cs
+++ WebRTCme.DemoApp/Blazor/WebRTCMe.DemoApp.Blazor.Wasm/Program.cs
@@ -3,10 +3,13 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Microsoft.JSInterop;
 using System;
 using System.Collections.Generic;
 using System.Net.Http;
+using System.Reflection;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace WebRTCme.DemoApp.Blazor.Wasm
@@ -18,14 +21,45 @@ namespace WebRTCme.DemoApp.Blazor.Wasm
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("#app");
 
+            ConfigureServices(builder.Services);
+
             builder.Services.AddScoped(sp => new HttpClient 
             { 
                 BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) 
             });
-
             builder.Services.AddBlazoredModal();
 
-            await builder.Build().RunAsync();
+            //await builder.Build().RunAsync();
+            var host = builder.Build();
+
+            ConfigureProviders(host.Services);
+
+            await host.RunAsync();
+        }
+
+        public static void ConfigureServices(IServiceCollection services)
+        {
+            services.AddOptions();
+        }
+
+        // To ignore null in JSInterop JSON globally.
+        public static void ConfigureProviders(IServiceProvider services)
+        {
+            try
+            {
+                var jsRuntime = services.GetService<IJSRuntime>();
+                var prop = typeof(JSRuntime).GetProperty("JsonSerializerOptions", 
+                    BindingFlags.NonPublic | BindingFlags.Instance);
+                JsonSerializerOptions value = (JsonSerializerOptions)Convert.ChangeType(
+                    prop.GetValue(jsRuntime, null), typeof(JsonSerializerOptions));
+                value.IgnoreNullValues = true;
+                value.PropertyNamingPolicy = JsonNamingPolicy.CamelCase;
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"EXCEPTION: {ex}");
+            }
         }
     }
 }
