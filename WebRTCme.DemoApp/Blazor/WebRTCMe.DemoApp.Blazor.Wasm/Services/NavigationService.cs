93742549acec89e583d839f9495fbe942145f39f
Melih Ercan <melihercan@gmail.com>
2021-02-17 19:01:49 +0300

    Wasm working again. Xamarin.Forms NuGwt were installed by mistake.

--- WebRTCme.DemoApp/Blazor/WebRTCMe.DemoApp.Blazor.Wasm/Services/NavigationService.cs
+++ WebRTCme.DemoApp/Blazor/WebRTCMe.DemoApp.Blazor.Wasm/Services/NavigationService.cs
@@ -1,22 +1,32 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Components;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using WebRTCme.Middleware;
-using Xamarin.Forms;
 
 namespace WebRTCme.DemoApp.Blazor.Wasm.Services
 {
     public class NavigationService : INavigationService
     {
-        public async Task NavigateToPageAsync(string uri)
+        private readonly NavigationManager _navigationManager;
+
+        public NavigationService(NavigationManager navigationManager)
         {
-            await Shell.Current.GoToAsync($"/{uri}");
+            _navigationManager = navigationManager;
         }
 
-        public async Task NavigateToPageAsync(string uri, string queryKey, string queryValue)
+        public Task NavigateToPageAsync(string url)
         {
-            await Shell.Current.GoToAsync($"/{uri}?{queryKey}={queryValue}");
+            _navigationManager.NavigateTo($"/{url}");
+            return Task.CompletedTask;
+        }
+
+        public Task NavigateToPageAsync(string url, string queryKey, string queryValue)
+        {
+            _navigationManager.NavigateTo($"/{url}/{queryValue}");
+            return Task.CompletedTask;
         }
     }
+
 }
--- WebRTCme.DemoApp/Xamarin/WebRTCme.DemoApp.Xamarin/Services/NavigationService.cs
+++ WebRTCme.DemoApp/Xamarin/WebRTCme.DemoApp.Xamarin/Services/NavigationService.cs
@@ -1,31 +1,23 @@
-﻿using Microsoft.AspNetCore.Components;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Text;
 using System.Threading.Tasks;
 using WebRTCme.Middleware;
+using Xamarin.Forms;
 
 namespace WebRTCme.DemoApp.Xamarin.Services
 {
     public class NavigationService : INavigationService
     {
-        private readonly NavigationManager _navigationManager;
-
-        public NavigationService(NavigationManager navigationManager)
+        public async Task NavigateToPageAsync(string uri)
         {
-            _navigationManager = navigationManager;
+            await Shell.Current.GoToAsync($"/{uri}");
         }
 
-        public Task NavigateToPageAsync(string url)
+        public async Task NavigateToPageAsync(string uri, string queryKey, string queryValue)
         {
-            _navigationManager.NavigateTo($"/{url}");
-            return Task.CompletedTask;
-        }
-
-        public Task NavigateToPageAsync(string url, string queryKey, string queryValue)
-        {
-            _navigationManager.NavigateTo($"/{url}/{queryValue}");
-            return Task.CompletedTask;
+            await Shell.Current.GoToAsync($"/{uri}?{queryKey}={queryValue}");
         }
     }
+
 }
