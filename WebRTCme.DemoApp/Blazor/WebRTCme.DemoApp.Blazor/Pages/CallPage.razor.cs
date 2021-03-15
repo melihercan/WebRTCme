d04c42f356804c1e9256055125a74e09a82d695c
Melih Ercan <melihercan@gmail.com>
2021-03-15 13:07:18 +0300

    Adding full project references.

--- WebRTCme.DemoApp/Blazor/WebRTCme.DemoApp.Blazor/Pages/CallPage.razor.cs
+++ WebRTCme.DemoApp/Blazor/WebRTCme.DemoApp.Blazor/Pages/CallPage.razor.cs
@@ -1,6 +1,5 @@
 ﻿using Blazored.Modal.Services;
 using Microsoft.AspNetCore.Components;
-using Microsoft.AspNetCore.SignalR.Client;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
