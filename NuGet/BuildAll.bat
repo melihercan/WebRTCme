951fd7b72d96d588775598dc59cedfcacbe80cd6
Melih Ercan <melihercan@gmail.com>
2021-03-16 13:18:45 +0300

    Simple script to build all NuGets.

---
+++ NuGet/BuildAll.bat
@@ -0,0 +1,5 @@
+nuget pack WebRTCme.Bindings.nuspec
+nuget pack WebRTCme.nuspec
+nuget pack WebRTCme.SignallingServerProxy.nuspec
+nuget pack WebRTCme.Middleware.Xamarin.nuspec
+nuget pack WebRTCme.Middleware.Blazor.nuspec
