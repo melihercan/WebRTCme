61dc5542e79518c1bf1c039578f42bdb2a8e9bc8
Melih Ercan <melihercan@gmail.com>
2021-03-16 16:44:57 +0300

    Cleanup.

--- NuGet/BuildAll.bat
+++
@@ -1,5 +0,0 @@
-nuget pack WebRTCme.Bindings.nuspec
-nuget pack WebRTCme.nuspec
-nuget pack WebRTCme.SignallingServerProxy.nuspec
-nuget pack WebRTCme.Middleware.Xamarin.nuspec
-nuget pack WebRTCme.Middleware.Blazor.nuspec
