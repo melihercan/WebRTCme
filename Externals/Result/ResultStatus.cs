b3fb6eb42e7de5085ad902e46dbc7a384ac23acd
Melih Ercan <melihercan@gmail.com>
2021-07-08 17:44:45 +0300

    Added Result project to Externals to fix the break in the build.

---
+++ Externals/Result/ResultStatus.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Utilme
+{
+    public enum ResultStatus
+    {
+        Ok,
+        Error,
+        NotFound,
+        Timeout,
+        Cancelled,
+        NotSupported,
+        InvalidData,
+    }
+}
---
+++ WebRTCme.MediaServer/WebRTCme.MediaSoupClient/Api/SdpCommonUtils.cs
@@ -0,0 +1,44 @@
+﻿using SDPLib;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using WebRTCme.ConnectionServer;
+
+namespace WebRTCme.MediaSoupClient.Api
+{
+    static class SdpCommonUtils
+    {
+        public static RtpCapabilities ExtractRtpCapabilities(SDP sdp)
+        {
+            Dictionary<int, RtpCodecCapability> codecsDictionary = new();
+            List<RtpHeaderExtension> headerExtensions = new();
+
+            bool gotAudio = false;
+            bool gotVideo = false;
+
+            foreach (var m in sdp.MediaDescriptions)
+            {
+                var kind = m.Media;
+                switch (kind)
+                {
+                    case "audio":
+                        if (gotAudio)
+                            continue;
+                        gotAudio = true;
+                        break;
+                    case "video":
+                        if (gotVideo)
+                            continue;
+                        gotVideo = true;
+                        break;
+                    default:
+                        continue;
+                }
+            }
+
+            //for (var )
+
+            return null;
+        }
+    }
+}
