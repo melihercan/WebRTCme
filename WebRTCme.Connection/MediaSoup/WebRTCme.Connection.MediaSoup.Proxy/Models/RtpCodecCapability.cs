bb0ba7366399faaac157ea67338b5e7c9866f03e
Melih Ercan <melihercan@gmail.com>
2021-10-03 14:58:26 +0300

    Correct order of mime and kind.

--- WebRTCme.Connection/MediaSoup/WebRTCme.Connection.MediaSoup.Proxy/Models/RtpCodecCapability.cs
+++ WebRTCme.Connection/MediaSoup/WebRTCme.Connection.MediaSoup.Proxy/Models/RtpCodecCapability.cs
@@ -8,8 +8,8 @@ namespace WebRTCme.Connection.MediaSoup
 {
     public class RtpCodecCapability
     {
-        public MediaKind Kind { get; set; }
         public string MimeType { get; init; }
+        public MediaKind Kind { get; set; }
         public int PreferredPayloadType { get; init; }
         public int ClockRate { get; init; }
         public int? Channels { get; set; }
