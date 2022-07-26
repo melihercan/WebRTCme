00876bce06028e982ee2d2aea65dcd636ca290e5
Melih Ercan <melihercan@gmail.com>
2022-07-26 21:53:46 +0300

    Maui Android binding.

---
+++ WebRTCme.Bindings/Maui/WebRTCme.Bindings.Maui.Android/Dummy.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Org.Webrtc
+{
+    public abstract partial class WrappedNativeVideoEncoder : global::Java.Lang.Object, global::Org.Webrtc.IVideoEncoder
+    {
+        public IVideoEncoder.ScalingSettings GetScalingSettings()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public partial class TextureBufferImpl : global::Java.Lang.Object, global::Org.Webrtc.VideoFrame.ITextureBuffer
+    {
+        VideoFrame.ITextureBuffer.Type VideoFrame.ITextureBuffer.GetType()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
