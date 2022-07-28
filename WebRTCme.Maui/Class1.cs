e3014ee8ac1c40fd0afe33f703793e0af04d3fc9
Melih Ercan <melihercan@gmail.com>
2022-07-28 11:05:53 +0300

    Effort to replace WebRTCme multu-platform project with Maui single project structure. Not working yet.

---
+++ WebRTCme.Maui/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace WebRTCme.Maui
+{
+    // All the code in this file is included in all platforms.
+    public class Class1
+    {
+    }
+}
---
+++ WebRTCme.Maui/IsExternalInit.cs
@@ -0,0 +1,5 @@
+﻿// Required to use C# 10.0.
+namespace System.Runtime.CompilerServices
+{
+    public class IsExternalInit { }
+}
