0e3da13e50d050aef342422cb3d78671e818d262
Melih Ercan <melihercan@gmail.com>
2022-07-30 11:40:55 +0300

    MAUI supporting Android and iOS.

--- WebRTCme.Middleware/WebRTCme.Middleware.Maui/Platforms/MacCatalyst/PlatformClass1.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace WebRTCme.Middleware.Maui
-{
-    // All the code in this file is only included on Mac Catalyst.
-    public class PlatformClass1
-    {
-    }
-}
--- WebRTCme.Middleware/WebRTCme.Middleware.Maui/Platforms/Windows/PlatformClass1.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace WebRTCme.Middleware.Maui
-{
-    // All the code in this file is only included on Windows.
-    public class PlatformClass1
-    {
-    }
-}
