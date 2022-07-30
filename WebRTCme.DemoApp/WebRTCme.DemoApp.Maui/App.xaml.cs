e39faa20ee27902c51f95ee77d8c0a4a598ecf50
Melih Ercan <melihercan@gmail.com>
2022-07-30 11:16:47 +0300

    Added MAUI Middleware and DemoApp projects (no implementation yet)

---
+++ WebRTCme.DemoApp/WebRTCme.DemoApp.Maui/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace WebRTCme.DemoApp.Maui
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
---
+++ WebRTCme.Middleware/WebRTCme.Middleware.Maui/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace WebRTCme.Middleware.Maui
+{
+    // All the code in this file is included in all platforms.
+    public class Class1
+    {
+    }
+}
