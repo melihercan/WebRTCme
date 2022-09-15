cc4cc0300bf86b81a0499f3d53040827f051f721
Melih Ercan <melihercan@gmail.com>
2022-09-15 19:36:03 +0300

    Desktop is in progress. Build is broken.

---
+++ WebRTCme/Platforms/Windows/Custom/WebRtc.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WebRTCme.Platforms.Windows.Custom
+{
+    internal class WebRtc
+    {
+    }
+}
---
+++ WebRTCme/Platforms/macOS/Custom/WebRtc.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WebRTCme.Platforms.macOS.Custom
+{
+    internal class WebRtc 
+    {
+    }
+}
