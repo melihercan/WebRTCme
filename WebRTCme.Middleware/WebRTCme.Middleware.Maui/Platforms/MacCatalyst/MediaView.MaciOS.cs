dce7a010e47158b1d3a33b6154430e544a45faa4
Melih Ercan <melihercan@gmail.com>
2026-04-19 19:55:57 +0300

    added windows

---
+++ WebRTCme.Middleware/WebRTCme.Middleware.Maui/Platforms/MacCatalyst/MediaView.MaciOS.cs
@@ -0,0 +1,12 @@
+using UIKit;
+
+namespace WebRTCme.Middleware
+{
+    public class MediaView : UIView
+    {
+        public void SetTrack(IMediaStreamTrack videoTrack)
+        {
+            throw new NotImplementedException("WebRTC is not implemented for MacCatalyst.");
+        }
+    }
+}
---
+++ WebRTCme.Middleware/WebRTCme.Middleware.Maui/Platforms/Windows/MediaView.Windows.cs
@@ -0,0 +1,10 @@
+namespace WebRTCme.Middleware
+{
+    public class MediaView : Microsoft.UI.Xaml.Controls.Grid
+    {
+        public void SetTrack(IMediaStreamTrack videoTrack)
+        {
+            throw new NotImplementedException("WebRTC is not implemented for Windows.");
+        }
+    }
+}
